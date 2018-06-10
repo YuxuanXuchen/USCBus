8b3cf9a01ac3acb06f40dbda34a6ccf028558979
Zefan Xu <zefanxu2@illinois.edu>
2018-06-09 23:49:36 -0700

    add Beautiful Soup implementation

--- python/constants.py
+++ python/constants.py
@@ -1,1 +1,4 @@
 uscBusesUrl = 'https://www.uscbuses.com/arrivals'
+uscBusesTextUrl = 'https://www.uscbuses.com/simple/routes'
+uscBusesRouteUrl = 'https://www.uscbuses.com/simple/routes/%s/stops'
+uscBusesRouteStopUrl = 'https://www.uscbuses.com/simple/routes/%s/stops/%s'
