7d169874470609e4c7f19cbc6988a5e2d6d20bb1
Yuki Chen <chen859@usc.edu>
2018-06-11 22:25:20 -0700

    use bs4 instead of selenium

--- python/beautifulSoup.py
+++ python/beautifulSoup.py
@@ -1,11 +1,12 @@
-import requests, constants, re
+import requests, constants, re, json
 from bs4 import BeautifulSoup
+
 
 class beautifulSoupFetcher:
     def __init__(self):
         self.routes = []
         self.stops = []
-        self.result = {}
+        self.result = []
 
     def getRoutes(self):
         startPage = requests.get(constants.uscBusesTextUrl)
@@ -19,7 +20,7 @@ class beautifulSoupFetcher:
         for eachRoute in self.routes:
             eachRouteStops = []
             routeId = eachRoute[1]
-            startPage = requests.get(constants.uscBusesRouteUrl%routeId)
+            startPage = requests.get(constants.uscBusesRouteUrl % routeId)
             soup = BeautifulSoup(startPage.content, 'html.parser')
             for eachHtml in soup.findAll("a"):
                 try:
@@ -34,19 +35,51 @@ class beautifulSoupFetcher:
 
     def getData(self):
         for eachRouteData in self.stops:
+            eachRouteResult = []
             routeId = eachRouteData[1]
             for eachStop in eachRouteData[2]:
                 stopId = eachStop[1]
                 startPage = requests.get(constants.uscBusesRouteStopUrl % (routeId, stopId))
                 soup = BeautifulSoup(startPage.content, 'html.parser')
+                eachStopResult = {"stopId": stopId, "stopName": eachStop[0]}
+                eachStopTimeResult = []
                 for eachHtml in soup.findAll("li"):
                     try:
                         eachHtml['class']
                     except:
-                        print eachHtml.get_text()
+                        dict = self.processData(eachHtml.get_text())
+                        if dict is not None:
+                            eachStopTimeResult.append(dict)
+                eachStopResult["time"] = eachStopTimeResult
+                eachRouteResult.append(eachStopResult)
+            self.result.append({"routeId": routeId, "routeName": eachStop[0], "routeTime": eachRouteResult})
+
+
+    def processData(self, s):
+        try:
+            busNum = re.search('Bus (.+?) arrive', s).group(1)
+            due = re.search('in (\d+?) minute', s).group(1)
+            arriveTime = re.search('at (.+?)\.', s).group(1)
+            return {"busNum": busNum, "due": due, "arriveTime": arriveTime}
+        except Exception as e:
+            return None
+
+    def run(self):
+        self.getRoutes()
+        self.getStops()
+        self.getData()
+
+    def getResult(self):
+        return json.dumps(self.result)
+
+    def cleanup(self):
+        self.routes = []
+        self.stops = []
+        self.result = []
 
 if __name__ == "__main__":
     b = beautifulSoupFetcher()
-    b.getRoutes()
-    b.getStops()
-    b.getData()
+    b.run()
+    print b.getResult()
+    # print b.processData("Bus A747 arrives in 2 minutes at 8:21 PM.")
+    # print b.processData("Arrival predictions are not available at this time.")
--- python/rest.py
+++ python/rest.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python2.7
 from flask import Flask
-import utils, threading, time, sys, signal, os
+import utils, beautifulSoup, threading, time, sys, signal, os
 import logging
 from logging.handlers import RotatingFileHandler
+from sys import platform
 
 app = Flask(__name__)
 lock = threading.Lock()
 result = ""
 
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.INFO)
-log_handler = RotatingFileHandler("app.log", mode='a', maxBytes=5*1024*1024,
-                                 backupCount=2, encoding=None, delay=0)
-log_handler.setLevel(logging.INFO)
-log.addHandler(log_handler)
+if platform == "linux" or platform == "linux2":
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.INFO)
+    log_handler = RotatingFileHandler("app.log", mode='a', maxBytes=5*1024*1024,
+                                     backupCount=2, encoding=None, delay=0)
+    log_handler.setLevel(logging.INFO)
+    log.addHandler(log_handler)
 
 @app.route('/')
 def index():
@@ -45,10 +47,24 @@ def getData():
             w = utils.webFetcher()
     w.cleanup()
 
+def getDataBS4():
+    global result
+    w = beautifulSoup.beautifulSoupFetcher()
+    while 1:
+        try:
+            w.run()
+            lock.acquire()
+            result = w.getResult()
+            lock.release()
+        except Exception as e:
+            print("Error: " + str(e))
+        finally:
+            w.cleanup()
+
 if __name__ == '__main__':
     thread1 = threading.Thread(target=runRest)
-    thread2 = threading.Thread(target=getData)
-    thread3 = threading.Thread(target=getData)
+    thread2 = threading.Thread(target=getDataBS4)
+    # thread3 = threading.Thread(target=getData)
     def signal_handler(signal, frame):
         print('\nKilling the server')
         os.kill(os.getpid(), 9)
@@ -56,7 +72,7 @@ if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal_handler)
     thread1.start()
     thread2.start()
-    time.sleep(10)
-    thread3.start()
+    # time.sleep(10)
+    # thread3.start()
     while 1:
         time.sleep(0.5)
